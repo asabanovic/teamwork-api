8268c30d45025cf90c503b360b52447d2d7bb1f9
Adnan Sabanovic <adnanxteam@gmail.com>
2017-05-22 11:17:27 +0000

    Refactoring user verification model/repo

--- app/Repositories/ActivationRepository.php
+++ app/Repositories/ActivationRepository.php
@@ -1,22 +1,21 @@
 <?php
 namespace App\Repositories;
 
-
+use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 use Illuminate\Database\Connection;
 use App\User;
 use Mail;
+use App\UserActivation;
 
-class ActivationRepository
+class ActivationRepository extends Model
 {
 
-    protected $db;
+    protected $activation;
 
-    protected $table = 'user_activations';
-
-    public function __construct(Connection $db)
+    public function __construct(UserActivation $activation = null)
     {
-        $this->db = $db;
+        $this->activation = $activation ?: new UserActivation;
     }
 
     protected function getToken()
@@ -26,21 +25,18 @@ class ActivationRepository
 
     public function createActivation($user)
     {
-
         $activation = $this->getActivation($user);
 
         if (!$activation) {
             return $this->createToken($user);
         }
         return $this->regenerateToken($user);
-
     }
 
     private function regenerateToken($user)
     {
-
         $token = $this->getToken();
-        $this->db->table($this->table)->where('user_id', $user->id)->update([
+        $this->activation->where('user_id', $user->id)->update([
             'token' => $token,
             'created_at' => new Carbon()
         ]);
@@ -53,7 +49,7 @@ class ActivationRepository
     private function createToken($user)
     {
         $token = $this->getToken();
-        $this->db->table($this->table)->insert([
+        $this->activation->insert([
             'user_id' => $user->id,
             'token' => $token,
             'created_at' => new Carbon()
@@ -66,18 +62,18 @@ class ActivationRepository
 
     public function getActivation($user)
     {
-        return $this->db->table($this->table)->where('user_id', $user->id)->first();
+        return $this->activation->where('user_id', $user->id)->first();
     }
 
 
     public function getActivationByToken($token)
     {
-        return $this->db->table($this->table)->where('token', $token)->first();
+        return $this->activation->where('token', $token)->first();
     }
 
     public function deleteActivation($token)
     {
-        $this->db->table($this->table)->where('token', $token)->delete();
+        $this->activation->where('token', $token)->delete();
     }
 
     public function activateUser($token)
@@ -100,14 +96,28 @@ class ActivationRepository
 
     }
 
+    public function getActivationLinkByUserId($user_id)
+    {      
+
+        $record = $this->activation->where('user_id', $user_id)->first(['token']);
+        return $this->getActivationLink($record->token);
+    }
+
+    public function getActivationLink($token)
+    {
+        return route('user.activate', ['token' => $token ]);
+    }
+
     public function sendToken($user, $token)
     {   
-        $link = route('user.activate', ['token' => $token ]);
-        
+        $link = $this->getActivationLink($token);
+
         Mail::send('email', ['link' => $link], function($message) use ($user)
         {
             $message->to($user->email)->subject('Please activate your account!');
         });
+
+        return $link;
     }
 
 }
